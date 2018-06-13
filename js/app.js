720314a8a7ee08d01beabbbc5ef3ccca0d36a84a
wietecha-mrzyglod <wietecha.katarzyna@gmail.com>
2018-06-13 14:11:44 +0200

    v-bind and v-on

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
 var data = {
-  items: ["Banany", "Jabłka"],
-  title: "Moja lista zakupów"
+  items: [
+    {text: "Banany", checked: true},
+    {text: "Jabłka", checked: false},
+    {text: "Zapałki", checked: true}
+  ],
+  title: "Moja lista zakupów",
+  newItem: ""
 };
 
 new Vue({
   el: "#app",
-  data: data
+  data: data,
+  methods: {
+    addItem: function(){
+      var text;
+      text = this.newItem.trim();
+      if(text){
+        this.items.push({
+          text: text,
+          checked: false
+        });
+        this.newItem = "";
+      }
+    }
+  }
 });
